28d8dfef9e21b5f1a3d6dbef15296ae7d18b04dc
Igor Akimov <akimoviigor@gmail.com>
2023-10-03 19:45:44 +0300

    add greeting

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,10 +1,26 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 import static hexlet.code.Cli.welcome;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        welcome();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("0 - Exit");
+        var choise = sc.nextInt();
+        System.out.println("Your choise: " + choise);
+        switch (choise) {
+            case 1:
+                System.out.println("Welcome to the Brain Games!");
+                welcome();
+                break;
+            default:
+                System.out.println("Goodbye!");
+
+        }
+
     }
 }
