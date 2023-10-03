d7c2d85bbed2628c0dbb9bba9facd78447397793
Igor Akimov <akimoviigor@gmail.com>
2023-10-03 10:26:39 +0300

    add in Cli welcome function remove welcome function fron App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,14 +1,10 @@
 package hexlet.code;
 
-import java.util.Scanner;
+import static hexlet.code.Cli.welcome;
 
 public class App {
     public static void main(String[] args) {
         System.out.println( "Welcome to the Brain Games!");
-        Scanner sc = new Scanner(System.in);
-        System.out.println("May I have your name? ");
-        var name = sc.next();
-        System.out.println("Hello, " + name + "!");
-
+        welcome();
     }
 }
---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,13 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+
+    public static void welcome() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("May I have your name? ");
+        var name = sc.next();
+        System.out.println("Hello, " + name + "!");
+    }
+}
