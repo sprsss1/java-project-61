76247368570df476095323b9490e30c41ed9499c
Igor Akimov <akimoviigor@gmail.com>
2023-10-03 10:18:49 +0300

    add in App "Hello"

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,14 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
         System.out.println( "Welcome to the Brain Games!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("May I have your name? ");
+        var name = sc.next();
+        System.out.println("Hello, " + name + "!");
+
     }
 }
