593a9dcb34eef20ddc5233a0d252c7b1cff15856
Igor Akimov <akimoviigor@gmail.com>
2023-10-04 15:27:56 +0300

    added game conditions

--- app/src/main/java/hexlet/code/Calc.java
+++ app/src/main/java/hexlet/code/Calc.java
@@ -1,7 +1,19 @@
 package hexlet.code;
 
+import static hexlet.code.Engine.*;
+
 public class Calc {
     public static void calcGame() {
+        String userName = welcome();
+        String gameTask = "What is the result of the expression?";
+        int firstNumber = randomIntGenerator();
+        int secondNumber = randomIntGenerator();
+        int answer = firstNumber + secondNumber;
+        String rightAnswer = String.valueOf(answer);
+        String question = firstNumber + " + " + secondNumber;
 
+
+        mainEngine(gameTask, question, rightAnswer, userName);
     }
+
 }
