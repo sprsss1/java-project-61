19e340d7b072941ecab2c5db11361667b58b3000
Igor Akimov <akimoviigor@gmail.com>
2023-10-04 15:26:28 +0300

    change Even: add random generator, variables

--- app/src/main/java/hexlet/code/Even.java
+++ app/src/main/java/hexlet/code/Even.java
@@ -1,14 +1,21 @@
 package hexlet.code;
 
 
-import static hexlet.code.Engine.mainEngine;
-import static hexlet.code.Engine.welcome;
+import static hexlet.code.Engine.*;
 
 public class Even {
     public static void evenGame() {
         String userName = welcome();
         String gameTask = "Answer 'yes' if the number is even, otherwise answer 'no'.";
+        int questNumber = randomIntGenerator();
 
-        mainEngine(gameTask, userName);
+        String rightAnswer = "yes";
+
+        if ((questNumber % 2) != 0) {
+            rightAnswer = "no";
+        }
+        String question = String.valueOf(questNumber);
+
+        mainEngine(gameTask, question, rightAnswer, userName);
     }
 }
