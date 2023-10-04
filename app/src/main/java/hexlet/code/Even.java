f3341122b0814707a93e062152e8dd546104b524
Igor Akimov <akimoviigor@gmail.com>
2023-10-04 14:51:18 +0300

    fix linter errors

--- app/src/main/java/hexlet/code/Even.java
+++ app/src/main/java/hexlet/code/Even.java
@@ -1,13 +1,14 @@
 package hexlet.code;
 
 
-import static hexlet.code.Engine.*;
+import static hexlet.code.Engine.mainEngine;
+import static hexlet.code.Engine.welcome;
 
 public class Even {
     public static void evenGame() {
         String userName = welcome();
         String gameTask = "Answer 'yes' if the number is even, otherwise answer 'no'.";
 
-        mainEngine(gameTask,userName);
+        mainEngine(gameTask, userName);
     }
 }
