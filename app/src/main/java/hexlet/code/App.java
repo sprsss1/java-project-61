18240620b57d941c7855f112f2e9280cc6dfd46f
Igor Akimov <akimoviigor@gmail.com>
2023-10-04 09:52:15 +0300

    add welcome in App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,25 +2,48 @@ package hexlet.code;
 
 import java.util.Scanner;
 
-import static hexlet.code.Cli.welcome;
+
+import static hexlet.code.Even.evenGame;
 
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
+        System.out.println("2 - Even");
         System.out.println("0 - Exit");
-        var choise = sc.nextInt();
+
+        int choise = 0;
+
+        if (sc.hasNextInt()) {
+            choise = sc.nextInt();
+        } else {
+            System.exit(1);
+        }
+        
         System.out.println("Your choise: " + choise);
+        System.out.println();
+
         switch (choise) {
             case 1:
-                System.out.println("Welcome to the Brain Games!");
                 welcome();
                 break;
+            case 2:
+                evenGame();
+                break;
             default:
-                System.out.println("Goodbye!");
+                System.exit(0);
+
 
         }
 
+    }
+    public static String welcome() {
+        System.out.println("Welcome to the Brain Games!");
+        Scanner scanName = new Scanner(System.in);
+        System.out.println("May I have your name? ");
+        var name = scanName.next();
+        System.out.println("Hello, " + name + "!");
+        return name;
     }
 }
