07ff9da7f59eaabc613e9b0097bb60a569420736
Igor Akimov <akimoviigor@gmail.com>
2023-10-04 09:52:44 +0300

    change welcomee

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,7 +1,7 @@
 package hexlet.code;
 import java.util.Scanner;
 public class Cli {
-    public static void welcome() {
+    public static void welcomee() {
         Scanner sc = new Scanner(System.in);
         System.out.println("May I have your name? ");
         var name = sc.next();
