27429adeb658281a726c409417625ec967d07e30
Igor Akimov <akimoviigor@gmail.com>
2023-10-04 17:58:10 +0300

    add answer/question generator

--- app/src/main/java/hexlet/code/Even.java
+++ app/src/main/java/hexlet/code/Even.java
@@ -1,24 +1,35 @@
 package hexlet.code;
 
-
-
 import static hexlet.code.Engine.welcome;
 import static hexlet.code.Engine.randomIntGenerator;
 import static hexlet.code.Engine.mainEngine;
+import static java.lang.Integer.parseInt;
 
 public class Even {
     public static void evenGame() {
         String userName = welcome();
         String gameTask = "Answer 'yes' if the number is even, otherwise answer 'no'.";
-        int questNumber = randomIntGenerator();
+        String[] question = new String[3];
 
-        String rightAnswer = "yes";
-
-        if ((questNumber % 2) != 0) {
-            rightAnswer = "no";
+        String[] rightAnswer = new String[3];
+        int questionCount = 3;
+        for (var i = 0; i < questionCount; i++) {
+            question[i] = questionGenerator();
+            rightAnswer[i] = answerGenerator(question[i]);
         }
-        String question = String.valueOf(questNumber);
 
         mainEngine(gameTask, question, rightAnswer, userName);
+    }
+    public static String questionGenerator() {
+        int fNumber = randomIntGenerator();
+        return String.valueOf(fNumber);
+    }
+    public static String answerGenerator(String string) {
+        int value = parseInt(string);
+        String answ = "yes";
+        if ((value % 2) != 0) {
+            answ = "no";
+        }
+        return answ;
     }
 }
