0e3051ec40d89cf112afc4cb07d8ee91d7f24b12
Igor Akimov <akimoviigor@gmail.com>
2023-10-05 19:49:31 +0300

    refactor app

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,64 +1,12 @@
 package hexlet.code;
 
-import java.util.Scanner;
-import static hexlet.code.Engine.welcome;
-import static hexlet.code.Even.evenGame;
-import static hexlet.code.Calc.calcGame;
-import static hexlet.code.Gcd.gcdGame;
-import static hexlet.code.Progression.progressionGame;
-import static hexlet.code.Prime.primeGame;
+import static hexlet.code.Engine.*;
 
 public class App {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter the game number and press Enter.");
-        System.out.println("1 - Greet");
-        System.out.println("2 - Even");
-        System.out.println("3 - Calc");
-        System.out.println("4 - GCD");
-        System.out.println("5 - Progression");
-        System.out.println("6 - Prime");
-        System.out.println("0 - Exit");
 
-        int choise = 0;
-
-        if (sc.hasNextInt()) {
-            choise = sc.nextInt();
-        } else {
-            System.out.println("Your choise is incorrect. Goodbye!");
-            System.exit(0);
-        }
-        System.out.println("Your choise: " + choise);
-        System.out.println();
-
-        switch (choise) {
-            case 0:
-                System.out.println("Goodbye!");
-                System.exit(0);
-            case 1:
-                welcome();
-                break;
-            case 2:
-                evenGame();
-                break;
-            case 3:
-                calcGame();
-                break;
-            case 4:
-                gcdGame();
-                break;
-            case 5:
-                progressionGame();
-                break;
-            case 6:
-                primeGame();
-                break;
-            default:
-                System.out.println("Your choise is incorrect. Goodbye!");
-                System.exit(0);
-
-
-        }
+        mainMenu();
+        launchGame();
 
     }
 
