4fa91372e18a87b6f65e3a10b3c970308eb4e8b7
Igor Akimov <akimoviigor@gmail.com>
2023-10-05 19:49:43 +0300

    refactor engine

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -2,7 +2,66 @@ package hexlet.code;
 
 import java.util.Scanner;
 
+import static hexlet.code.Calc.calcGame;
+import static hexlet.code.Even.evenGame;
+import static hexlet.code.Gcd.gcdGame;
+import static hexlet.code.Prime.primeGame;
+import static hexlet.code.Progression.progressionGame;
+
 public class Engine {
+    public static void mainMenu() {
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("3 - Calc");
+        System.out.println("4 - GCD");
+        System.out.println("5 - Progression");
+        System.out.println("6 - Prime");
+        System.out.println("0 - Exit");
+    }
+    public static void launchGame() {
+        Scanner sc = new Scanner(System.in);
+
+        int choise = 0;
+
+        if (sc.hasNextInt()) {
+            choise = sc.nextInt();
+        } else {
+            System.out.println("Your choise is incorrect. Goodbye!");
+            System.exit(0);
+        }
+        System.out.println("Your choise: " + choise);
+        System.out.println();
+
+        switch (choise) {
+            case 0:
+                System.out.println("Goodbye!");
+                System.exit(0);
+            case 1:
+                welcome();
+                break;
+            case 2:
+                evenGame();
+                break;
+            case 3:
+                calcGame();
+                break;
+            case 4:
+                gcdGame();
+                break;
+            case 5:
+                progressionGame();
+                break;
+            case 6:
+                primeGame();
+                break;
+            default:
+                System.out.println("Your choise is incorrect. Goodbye!");
+                System.exit(0);
+
+
+        }
+    }
     public static String welcome() {
         System.out.println("Welcome to the Brain Games!");
         Scanner scanName = new Scanner(System.in);
