195032b05437134fc1d6300679c7c065710228d4
Igor Akimov <akimoviigor@gmail.com>
2023-10-05 19:51:31 +0300

    fix linter errors

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,6 +1,7 @@
 package hexlet.code;
 
-import static hexlet.code.Engine.*;
+import static hexlet.code.Engine.launchGame;
+import static hexlet.code.Engine.mainMenu;
 
 public class App {
     public static void main(String[] args) {
